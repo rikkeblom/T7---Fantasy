94a88284c7ca60eb74cfa28bfc90871413672e55
Rikke Blom <rikk957v@stud.kea.dk>
2021-02-21 19:38:33 +0100

    Pronouns-and-background

--- js/random.js
+++ js/random.js
@@ -3,6 +3,7 @@ const urlRace = "https://kea21-7e1e.restdb.io/rest/race";
 const urlMaleName = "https://kea21-7e1e.restdb.io/rest/malename";
 const urlFemaleName = "https://kea21-7e1e.restdb.io/rest/femalename";
 const urlOccupation = "https://kea21-7e1e.restdb.io/rest/occupation";
+const urlBackground = "https://kea21-7e1e.restdb.io/rest/background";
 const options = {
   headers: {
     "x-apikey": "602f9e445ad3610fb5bb63d5",
@@ -37,13 +38,27 @@ function randomOccupation(occupation) {
     occupation[Math.floor(Math.random() * occupation.length)].x;
 }
 
+//random background-------------------------
+fetch(urlBackground, options)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    randomBackground(data);
+  });
+
+function randomBackground(background) {
+  document.querySelector("#background").textContent =
+    background[Math.floor(Math.random() * background.length)].x;
+}
+
 //random gender-------------------------
 const genderplace = document.querySelector(".gender");
 const charactergender = gender[Math.floor(Math.random() * gender.length)];
 genderplace.textContent = charactergender;
 randomName();
-//random name (based on gender)----------------------
 
+//random name (based on gender)----------------------
 function randomName() {
   if (charactergender == "female") {
     console.log("female name");
@@ -72,6 +87,8 @@ function randomFemaleName(name) {
   a[0].textContent = b;
   a[1].textContent = b;
   console.log(b);
+  const c = document.querySelectorAll(".pronoun");
+  c[0].textContent = "she";
 }
 
 function randomMaleName(name) {
@@ -80,4 +97,6 @@ function randomMaleName(name) {
   a[0].textContent = b;
   a[1].textContent = b;
   console.log(b);
+  const c = document.querySelectorAll(".pronoun");
+  c[0].textContent = "he";
 }
