e145ac569066a41a6f94f23c2d73fadc7a3c5aed
Rikke Blom <rikk957v@stud.kea.dk>
2021-02-24 10:51:43 +0100

    first-try-now-with-repository-name-in-image-link

--- js/random.js
+++ js/random.js
@@ -363,29 +363,33 @@ function changeImage() {
     if (document.querySelector(".gender").textContent == "male") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body human MALE image");
-        document.querySelector(".img-avatar").src = "../img/human-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/human-boy.svg";
       } else {
         console.log("Insert bust of human MALE image");
-        document.querySelector(".img-avatar").src = "../img/human-boy-bust.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/human-boy-bust.svg";
       }
     } else if (document.querySelector(".gender").textContent == "female") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body human FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/human-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/human-girl.svg";
       } else {
         console.log("Insert BUST of human FEMALE image");
         document.querySelector(".img-avatar").src =
-          "../img/human-girl-bust.svg";
+          "T7---Fantasy/img/human-girl-bust.svg";
       }
     } else {
       console.log("Insert human NON-BINARY image");
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body human NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/human-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/human-girl.svg";
       } else {
         console.log("Insert bust of human NON-BINARY image");
         document.querySelector(".img-avatar").src =
-          "../img/human-girl-bust.svg";
+          "T7---Fantasy/img/human-girl-bust.svg";
       }
     }
     //----------------DWARF-----------------DONE--
@@ -393,26 +397,32 @@ function changeImage() {
     if (document.querySelector(".gender").textContent == "male") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body dwarf MALE image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-boy.svg";
       } else {
         console.log("Insert BUST of dwarf MALE image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-boy.svg";
       }
     } else if (document.querySelector(".gender").textContent == "female") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body dwarf FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-girl.svg";
       } else {
         console.log("Insert BUST of dwarf FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-girl.svg";
       }
     } else {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body dwarf NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-boy.svg";
       } else {
         console.log("Insert bust of dwarf NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/dwarf-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/dwarf-boy.svg";
       }
     }
     //-------------TIEFLING-----------DONE--
@@ -420,29 +430,32 @@ function changeImage() {
     if (document.querySelector(".gender").textContent == "male") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body tiefling MALE image");
-        document.querySelector(".img-avatar").src = "../img/tiefling-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/tiefling-boy.svg";
       } else {
         console.log("Insert BUST of tiefling MALE image");
         document.querySelector(".img-avatar").src =
-          "../img/tiefling-boy-bust.svg";
+          "T7---Fantasy/img/tiefling-boy-bust.svg";
       }
     } else if (document.querySelector(".gender").textContent == "female") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body tiefling FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/tiefling-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/tiefling-girl.svg";
       } else {
         console.log("Insert BUST of tiefling FEMALE image");
         document.querySelector(".img-avatar").src =
-          "../img/tiefling-girl-bust.svg";
+          "T7---Fantasy/img/tiefling-girl-bust.svg";
       }
     } else {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body tiefling NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/tiefling-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/tiefling-boy.svg";
       } else {
         console.log("Insert bust of tiefling NON-BINARY image");
         document.querySelector(".img-avatar").src =
-          "../img/tiefling-boy-bust.svg";
+          "T7---Fantasy/img/tiefling-boy-bust.svg";
       }
     }
 
@@ -451,26 +464,32 @@ function changeImage() {
     if (document.querySelector(".gender").textContent == "male") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body elf MALE image");
-        document.querySelector(".img-avatar").src = "../img/elf-boy.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-boy.svg";
       } else {
         console.log("Insert BUST of elf MALE image");
-        document.querySelector(".img-avatar").src = "../img/elf-boy-bust.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-boy-bust.svg";
       }
     } else if (document.querySelector(".gender").textContent == "female") {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body elf FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/elf-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-girl.svg";
       } else {
         console.log("Insert BUST of elf FEMALE image");
-        document.querySelector(".img-avatar").src = "../img/elf-girl-bust.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-girl-bust.svg";
       }
     } else {
       if (window.innerWidth > 520 == true) {
         console.log("Insert Full body elf NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/elf-girl.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-girl.svg";
       } else {
         console.log("Insert bust of elf NON-BINARY image");
-        document.querySelector(".img-avatar").src = "../img/elf-girl-bust.svg";
+        document.querySelector(".img-avatar").src =
+          "T7---Fantasy/img/elf-girl-bust.svg";
       }
     }
 
